9c5ed9052e0ee338b2c292e1bb8d90d101d44d07
anmol0001 <anmol0556.cse19@chitkara.edu.in>
2021-10-10 17:20:42 +0530

    Food_Menu

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';       
+import ImageGallery from './foodWebsite/ImageGallery';
+
+ReactDom.render(<ImageGallery/>
+    ,document.getElementById("root"));
